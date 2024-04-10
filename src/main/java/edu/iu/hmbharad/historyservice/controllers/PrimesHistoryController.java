5de3844a2c5ee539a1d8f24cfc593ea6c690ef74
Hrishikesh Bharadwaj <48100950+easycase00@users.noreply.github.com>
2024-04-09 22:37:52 -0400

    Practicum13 history

---
+++ src/main/java/edu/iu/hmbharad/historyservice/controllers/PrimesHistoryController.java
@@ -0,0 +1,24 @@
+package edu.iu.hmbharad.historyservice.controllers;
+
+import edu.iu.hmbharad.historyservice.model.PrimesRecord;
+import edu.iu.hmbharad.historyservice.repository.PrimesHistoryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+public class PrimesHistoryController {
+    private final PrimesHistoryRepository primesHistoryRepository;
+
+    public PrimesHistoryController(PrimesHistoryRepository primesHistoryRepository) {
+        this.primesHistoryRepository = primesHistoryRepository;
+    }
+
+    @GetMapping("/{customer}")
+    public List<PrimesRecord> findAll(@PathVariable String customer) {
+        return primesHistoryRepository.findAllByCustomer(customer);
+    }
+}
+
